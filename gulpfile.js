9487f38eed2169a0d1f30afa864063d11f2d6566
bonn <dev@intelligenttechnologies.com.au>
2022-01-29 17:34:22 +0800

    Header Style, Creating Style Settings

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')(require('node-sass'));
+const themeKit = require('@shopify/themekit')
 
 gulp.task('convert-sass', async function () {
-    return await gulp.src('src/css/main.scss').pipe(sass()).pipe(gulp.dest('assets/custom-style.css'));
+    return await gulp.src('src/css/main.scss').pipe(sass()).pipe(gulp.dest('assets'));
 })
 
 gulp.task('watch', function () {
-    gulp.watch('src/css/main.scss', gulp.series('convert-sass'))
+    gulp.watch('src/css/**/*.scss', gulp.series('convert-sass'))
+    themeKit.command('watch', { env: 'development', 'allow-live': true })
 })
